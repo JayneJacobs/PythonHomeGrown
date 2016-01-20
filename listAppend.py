31e931053c3e1d3e8fe5d339b4292e9c595941b1
JayneJacobs <jaynejacobs@comcast.net>
2016-01-20 11:13:29 -0500

    Initial commit  of code

---
+++ listAppend.py
@@ -0,0 +1,24 @@
+p=['Moe', 'Larry', 'Shemp']
+q=p
+
+print q
+q[2]='!'
+
+print q
+
+print p
+p[2]='Curly'
+
+print q
+q[2]='Curly'
+print p
+p=[0,0,7]
+
+
+print p , q
+
+spy = p
+
+agent = spy
+spy[2] = agent[2]+1
+print agent , spy , q
---
+++ speedoflightms.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+speedOfLightms = 299792458
+billionth = 1.0 / 10000000000
+nanostick = speedOfLightms * billionth
